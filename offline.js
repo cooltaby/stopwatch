c6d4d0bb3a953e01ae190fd488392088b4919c4d
cooltaby <cooltaby@users.noreply.github.com>
2019-12-30 16:55:08 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1577704894,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/stopwatch-sheet0.png",
+		"images/spritelap-sheet0.png",
+		"images/spritereset-sheet0.png",
+		"images/spritestart-sheet0.png",
+		"images/spritestop-sheet0.png",
+		"images/spritefontstopwatch.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"loading-logo.png",
+		"icon-256.png"
+	]
+}
